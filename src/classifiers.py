1a3a35e93ed78f222a8c835c1c7b4d41da6f578b
Michelle Lieng <michellelieng2020@gmail.com>
2025-06-05 13:35:35 +1000

    non refactored data

---
+++ src/classifiers.py
@@ -0,0 +1,23 @@
+SCALE_UPS = {
+    "Canva", "SafetyCulture", "Airwallex", "Linktree", "Employment Hero",
+    "Immutable", "Atlassian", "Culture Amp", "Go1", "Eucalyptus", "Qwilr",
+    "Mr Yum", "Vow", "Lyka", "Finder", "Xero", "Cover Genius", "Forage",
+    "Halaxy", "Brighte", "Zeller", "Dovetail", "Milkrun", "Spaceship",
+    "Fluency", "HappyCo", "Carted", "Shippit", "Jigspace", "Humanitix", 
+    "Relevance AI", "Atomi"
+}
+
+BIG_TECH = {
+    "Google", "Meta", "Facebook", "Amazon", "Apple", "Microsoft", "Netflix",
+    "Nvidia", "Adobe", "IBM", "Oracle", "LinkedIn", "Dropbox", "Salesforce",
+    "Palantir", "Stripe", "Shopify", "Twilio", "Uber", "Airbnb", "Square",
+    "Pinterest", "Reddit", "TikTok", "ByteDance", "Snap", "Tesla", "Intel"
+}
+
+ACCELERATORS = {
+    "Startmate", "Antler", "UNSW Founders", "Melbourne Accelerator Program",
+    "The Generator", "Remarkable", "Blackbird Giants", "Blackbird", "Moonshot", "LaunchVic",
+    "Techstars", "Y Combinator", "YC", "500 Startups", "EF", "On Deck", "Skalata",
+    "H2 Ventures", "Spiral Blue", "Stone & Chalk", "Tank Stream Labs",
+    "MAP", "Slingshot", "Austrade Landing Pads", "Build Club", "The Builders Club"
+}
